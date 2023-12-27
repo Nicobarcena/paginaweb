d1ecce677eefce53f0f848f1cae3a903a6eaed59
Nicobarcena <120732193+Nicobarcena@users.noreply.github.com>
2023-12-27 20:02:54 -0300

    componentes

---
+++ src/Componentes/screens/Benedits.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Benedits = () => {
+  return (
+    <div>Benedits</div>
+  )
+}
+
+export default Benedits
---
+++ src/Componentes/screens/HowWeWorks.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HowWeWorks = () => {
+  return (
+    <div>HowWeWorks</div>
+  )
+}
+
+export default HowWeWorks
