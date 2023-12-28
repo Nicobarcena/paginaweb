345ca6cf8eee2ef9541575d742de546bbcdafe54
Nicobarcena <120732193+Nicobarcena@users.noreply.github.com>
2023-12-27 22:13:38 -0300

    nav

--- src/Componentes/screens/Comp/Nav.jsx
+++ src/Componentes/screens/Comp/Nav.jsx
@@ -1,18 +1,43 @@
-import React from 'react'
-import {Links} from "react-scroll";
+import React, { useEffect, useState } from 'react'
+import {Link} from "react-scroll";
+import "./Nav.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faX } from '@fortawesome/free-solid-svg-icons'
+
 const Nav = () => {
-const links =[
+const [navBarOpen, setNavBarOpen] = useState (false)
+const [windowDimension, setWindowDimension] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+    });
+
+    const detectDimension = ()=>{
+        setWindowDimension({
+            width: window.innerWidth,
+            height: window.innerHeight
+        })
+    }
+
+    useEffect(()=>{
+        window.addEventListener("resize", detectDimension)
+        windowDimension.width > 800 && setNavBarOpen(false)
+        return () =>{
+            window.removeEventListener ("resize", detectDimension)
+        }
+    },[windowDimension])
+
+    const links =[
     {
         id:1,
         link: "Home",
     },
     {
         id:2,
-        link:"Services",
+        link:"Servicios",
     },
     {
         id:3,
-        link:"HomeWeWok",
+        link:"HowWeWorks",
     },
     {
         id:4,
@@ -21,12 +46,27 @@ const links =[
 ]
 
 return (
-    <div>
+    <div className= {!navBarOpen ? "Navbar" : "NavOpen" }>
+        {!navBarOpen && <p className='logo'>fojazero</p>}
+        {!navBarOpen ? <FontAwesomeIcon size= {100} icon={faBars} onClick={() => setNavBarOpen(!navBarOpen)}  />
+        : <FontAwesomeIcon  icon={faX} onClick={() => setNavBarOpen(!navBarOpen)} color='#f1f1f1'  /> }
+        {
+        navBarOpen && 
         <ul>
-            {links.map(x =>(
-                <links> {x.link === "HomeWeWork" ? "How We Work" :x.link} </links>
-            ))}
-        </ul>
+        {links.map((x)=>{
+            return (
+                <div>
+                    <Link
+                    onClick={()=>setNavBarOpen(false)}
+                    to={x.link}
+                    smooth duration={500} 
+                    className='navlink'> {x.link === "HowWeWorks" ? "How We Works": x.link} </Link>
+                    <div className='border' ></div>
+                </div>
+            )
+        } )}
+    </ul>
+        }
     </div>
     )
 }
