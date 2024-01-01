ad5dbe702f829f976dd9bc7cbfccfc4936f8e9bc
Nicobarcena <120732193+Nicobarcena@users.noreply.github.com>
2024-01-01 01:03:24 -0300

    ds

--- src/Componentes/screens/Comp/Nav.jsx
+++ src/Componentes/screens/Comp/Nav.jsx
@@ -3,72 +3,52 @@ import {Link} from "react-scroll";
 import "./Nav.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faX } from '@fortawesome/free-solid-svg-icons'
-
-const Nav = () => {
-const [navBarOpen, setNavBarOpen] = useState (false)
-const [windowDimension, setWindowDimension] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-    });
-
-    const detectDimension = ()=>{
-        setWindowDimension({
-            width: window.innerWidth,
-            height: window.innerHeight
-        })
-    }
-
-    useEffect(()=>{
-        window.addEventListener("resize", detectDimension)
-        windowDimension.width > 800 && setNavBarOpen(false)
-        return () =>{
-            window.removeEventListener ("resize", detectDimension)
-        }
-    },[windowDimension])
-
-    const links =[
-    {
-        id:1,
-        link: "Home",
-    },
-    {
-        id:2,
-        link:"Servicios",
-    },
-    {
-        id:3,
-        link:"HowWeWorks",
-    },
-    {
-        id:4,
-        link:"Benedits"
-    }
-]
-
-return (
-    <div className= {!navBarOpen ? "Navbar" : "NavOpen" }>
-        {!navBarOpen && <p className='logo'>fojazero</p>}
-        {!navBarOpen ? <FontAwesomeIcon size= {100} icon={faBars} onClick={() => setNavBarOpen(!navBarOpen)}  />
-        : <FontAwesomeIcon  icon={faX} onClick={() => setNavBarOpen(!navBarOpen)} color='#f1f1f1'  /> }
+const Nav = ()=>{
+    const [navBarOpen, setNavBarOpen] = useState (false)
+    const links = [
         {
-        navBarOpen && 
-        <ul>
-        {links.map((x)=>{
-            return (
-                <div>
-                    <Link
-                    onClick={()=>setNavBarOpen(false)}
-                    to={x.link}
-                    smooth duration={500} 
-                    className='navlink'> {x.link === "HowWeWorks" ? "How We Works": x.link} </Link>
-                    <div className='border' ></div>
-                </div>
-            )
-        } )}
-    </ul>
+            id:1,
+            link:"Home",
+        },
+        {
+            id:2,
+            link:"Servicos",
+        },
+        {
+            id:3,
+            link:"HowWeWork",
+        },
+        {
+            id:4,
+            link:"Benedits",
+        },
+        {
+            id:5,
+            link:"Contacto",
         }
-    </div>
-    )
-}
+    ]
+
+return ( 
+
+<div className= {!navBarOpen ? "Navbar" : "NavOpen" }>
+    {!navBarOpen && <p className=''>Foja Zero</p> }
+    
+        {!navBarOpen ? 
+        (  <FontAwesomeIcon  icon={faBars} onClick={() => setNavBarOpen(!navBarOpen)}  />
+        ): (  
+        <FontAwesomeIcon  icon={faX} onClick={() => setNavBarOpen(!navBarOpen)}   />
+        )}
+    {
+        navBarOpen && <ul>
+        {links.map(x => (
+            <div>
+                <Link> {x.link === "HowWeWork" ? "How We Work" : x.link} </Link>
+            </div>
+        ))}
+    </ul>
+    }
+</div>
+)
+};
 
 export default Nav
