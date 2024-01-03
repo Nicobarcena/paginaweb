5d3a48332674368a0badf9abca3c8ec2daeaeaa6
Nicobarcena <120732193+Nicobarcena@users.noreply.github.com>
2024-01-02 22:05:12 -0300

    cacambios

--- src/Componentes/screens/Comp/Nav.jsx
+++ src/Componentes/screens/Comp/Nav.jsx
@@ -3,8 +3,29 @@ import {Link} from "react-scroll";
 import "./Nav.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faX } from '@fortawesome/free-solid-svg-icons'
+
 const Nav = ()=>{
     const [navBarOpen, setNavBarOpen] = useState (false)
+    const [windowDimension, setWindowDimension] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+
+    const detectDimension = ()=>{
+        setWindowDimension({
+            width: window.innerWidth,
+            height: window.innerHeight,
+        })
+    }
+
+    useEffect(()=>{
+        window.addEventListener ("resize", detectDimension)
+        windowDimension.width >800 && setNavBarOpen(false)
+        return ()=>{
+            window.removeEventListener("resize", detectDimension)
+        }
+    },[windowDimension])
+
     const links = [
         {
             id:1,
@@ -28,27 +49,41 @@ const Nav = ()=>{
         }
     ]
 
-return ( 
-
-<div className= {!navBarOpen ? "Navbar" : "NavOpen" }>
-    {!navBarOpen && <p className=''>Foja Zero</p> }
-    
-        {!navBarOpen ? 
-        (  <FontAwesomeIcon  icon={faBars} onClick={() => setNavBarOpen(!navBarOpen)}  />
-        ): (  
-        <FontAwesomeIcon  icon={faX} onClick={() => setNavBarOpen(!navBarOpen)}   />
-        )}
-    {
-        navBarOpen && <ul>
-        {links.map(x => (
-            <div>
-                <Link> {x.link === "HowWeWork" ? "How We Work" : x.link} </Link>
+    return ( 
+        <div className= {!navBarOpen ? "Navbar" : "NavOpen" }>
+            {!navBarOpen && <p className='logo'>Foja Zero</p> }
+            
+            {!navBarOpen && windowDimension.width <800  ? 
+            ( <FontAwesomeIcon icon={faBars} onClick={() => setNavBarOpen(!navBarOpen)} />
+            ): windowDimension.width <800 &&
+                (  
+            <FontAwesomeIcon color='#f1f1f1f1' icon={faX} onClick={() => setNavBarOpen(!navBarOpen)}   />
+            )}
+            {navBarOpen || (
+                windowDimension.width > 800 ||
+                (windowDimension.width < 800 && (
+                <ul className='linksconteiner' > 
+                    {links.map((x) => {
+            return (
+                <div>
+                    <Link
+                    onClick={() => setNavBarOpen(false)}
+                    to={x.link}
+                    smooth
+                    duration={500}
+                    className={"navLink"}
+                    >
+                    {x.link === "HowWeWork" ? "How We Work" : x.link}
+                    </Link>
+            <div className="border"></div>
             </div>
-        ))}
+        );
+        })}
     </ul>
-    }
-</div>
-)
+    ))
+)}
+        </div>
+    )
 };
 
 export default Nav
